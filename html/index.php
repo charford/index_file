ede4b7da2aee23c6a338b4096160df701ad84fa2
Casey Harford <caseyharford@Caseys-MacBook-Pro.local>
2014-06-26 10:56:24 -0700

    index.php

---
+++ html/index.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+        <title>Hello World</title>
+</head>
+<body>
+        <h1>Look at this great webpage</h1>
+
+        <h2>Users in the db listed below</h2>
+        <?php
+        $con=mysqli_connect(db,"webuser","password1","test");
+        // Check connection
+        if (mysqli_connect_errno()) {
+          echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        }
+
+        $result = mysqli_query($con,"SELECT * FROM users");
+
+        while($row = mysqli_fetch_array($result)) {
+          echo $row['first_name'] . " " . $row['last_name'];
+          echo "<br>";
+        }
+
+        mysqli_close($con);
+        ?>
+        <?php phpinfo() ?>
+</body>
+</html>
